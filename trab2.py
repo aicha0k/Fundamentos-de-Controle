f075db46a2f1b03b86b58b46d794c89f0962f515
Aicha AL-Rob <aicha.k2002@gmail.com>
2025-11-19 23:01:32 -0300

    desenvolvimento até agora

--- trab2.py
+++ trab2.py
@@ -283,7 +283,243 @@ def executar_PC48():
     print("conclusao:\n O controlador proporcional PI reduz, mas nao elimina o erro em regime estacionario.")
     print("O controlador PI adiciona um polo na origem, o que aumenta o tipo do sistema de 0 para 1 e consequente o erro em regime estacionario é nulo.")
     
+def sistema_PC411(K):
+    C = ctrl.TransferFunction([K], [1]) # controlador
+    P = ctrl.TransferFunction([20], [1, 4.5, 64]) # processo
+    H = ctrl.TransferFunction([1], [1, 1]) # sensor
+    G = C * P # malha aberta
+    T = ctrl.feedback(G, H) # malha fechada com sensor
+    return T, G, H
     
+def executar_PC411(K_values=[10,12,15], tempo_final=10):
+    plt.figure()
+    results = []
+    for K in K_values:
+        T, G, H = sistema_PC411(K)
+        t, y = resposta_ao_degrau(T, tempo_final=tempo_final)
+        
+        plt.plot(t, y, label=f'K={K}')
+        Mp, ts, info = desempenho_sistema(T)
+        ess = erro_estacionario(G, tipo="degrau")
+        results.append({
+            "K": K,
+            "Mp": Mp,
+            "ts": ts,
+            "ess": ess
+        })
+    plt.title('Resposta ao Degrau para Diferentes Valores de K')
+    plt.xlabel('tempo (s)')
+    plt.ylabel('saída y(t)')
+    plt.grid(True)
+    plt.legend()
+    plt.show()
+    print("resumo de resultados PC411:")
+    for res in results:
+        print(f"K={res['K']}")
+        print(f"Mp={res['Mp']:.3f}%")
+        print(f"Tempo de acomodação: {res['ts']:.3f}s")
+        print(f"Erro em regime estacionario: {res['ess']:.3f}")
+        print("-----------------")
+        
+    return results
+   
+def criar_L():
+    s = ctrl.TransferFunction.s
+    L = (s + 10) / (s**2 * (s + 15))
+    return L
+
+def resposta_rampa(T, tempo_final=50, n_pontos=2000):
+    t = np.linspace(0, tempo_final, n_pontos)
+    r = t # rampa unitaria r(t) = t
+    t_out, y = ctrl.forced_response(T, T=t, U=r)
+    e = r - y
+    ess_apox = e[-1]
+    return t_out, r, y, e, ess_apox
+
+def executar_rampa_L():
+    L = criar_L()
+    T = ctrl.feedback(L, 1) # malha fechada, realimentacao unitaria
+    t,r, y, e, ess_apox = resposta_rampa(T)
+    
+    plt.figure()
+    plt.plot(t, y, label='Resposta à Rampa')
+    plt.axhline(1, color='r', linestyle='--', label='Entrada rampa (t)')
+    plt.axhline(ess_apox, color='g', linestyle='--', label=f'Erro em regime estacionario: {ess_apox:.3f}')
+    plt.title('Resposta à Rampa do Sistema em Malha Fechada')
+    plt.xlabel('Tempo (s)')
+    plt.ylabel('Saída y(t)')
+    plt.grid(True)
+    plt.legend()
+    
+    # plot erro
+    plt.figure()
+    plt.plot(t, e, label='Erro')
+    # plt.axhline(0, color='b', linestyle='--', label='Erro nulo')
+    plt.title('Erro em Regime Estacionário PC5.2')
+    plt.xlabel('Tempo (s)')
+    plt.ylabel('Erro e(t)')
+    plt.grid(True)
+    plt.legend()
+    plt.show()
+    print(f"Erro em regime estacionario: {ess_apox:.6f}")
+    
+def PC54_sistema():
+    s = ctrl.TransferFunction.s
+    Gc = 21/s
+    Gp = 1/(s+2)
+    L = Gc * Gp
+    T = ctrl.feedback(L, 1)
+    return T
+def calcular_MUP(T):
+    info = ctrl.step_info(T)
+    Mp = info['Overshoot']
+    return Mp
+
+def plotar_resposta_PC54():
+    T = PC54_sistema()
+    t, y = ctrl.step_response(T, np.linspace(0, 10, 2000))
+
+    plt.figure()
+    plt.plot(t, y, label="Resposta ao degrau")
+    plt.axhline(1, color='r', linestyle='--', label="Valor final = 1")
+    plt.title("Resposta ao Degrau do PC5.4")
+    plt.xlabel("Tempo (s)")
+    plt.ylabel("Saída y(t)")
+    plt.grid(True)
+    plt.legend()
+    plt.show()
+
+    # cálculo aproximado visual
+    y_max = max(y)
+    MUP_aprox = (y_max - 1) * 100
+    print(f"y_max = {y_max:.3f}")
+    print(f"MUP (estimada pelo gráfico) = {MUP_aprox:.2f}%")
+
+def PC58_sistema():
+    s = ctrl.TransferFunction.s
+    Gc = (0.1*s + 5) / s
+    Gp = 100*(s+1)/ (s**2 + 2*s + 100)
+    
+    L = Gc * Gp
+    T = ctrl.feedback(L, 1)
+    return T, L
+
+def parametros_PC58(T):
+    den = T.den[0][0]
+    polos = np.roots(den)
+    polos_ordenados = np.sort(polos)
+    p1, p2 = polos_ordenados[:2] # polos dominantes
+    print(f"Polos dominantes: {p1:.3f}, {p2:.3f}")
+    # parâmetros equivalentes
+    wn = np.sqrt(p1.real**2 + p1.imag**2)
+    zeta = -p1.real / wn
+    print(f"Frequência natural: {wn:.3f}")
+    print(f"Coeficiente de amortecimento: {zeta:.3f}")
+    return wn, zeta
+
+def calcular_PC58(zeta, wn):
+    Mp=np.exp(-np.pi*zeta/np.sqrt(1-zeta**2))*100
+    Tp = np.pi / (wn * np.sqrt(1-zeta**2))
+    Ts = 4 / (zeta * wn)
+    return Mp, Tp, Ts
+
+def simular_PC58():
+    T,L = PC58_sistema()
+    t,y = ctrl.step_response(T, np.linspace(0, 5, 2000))
+    info = ctrl.step_info(T)
+    
+    Mp = info['Overshoot']
+    Tp = info['PeakTime']
+    Ts = info['SettlingTime']
+    
+    wn, zeta = parametros_PC58(T)
+    Mp_analitico, Tp_analitico, Ts_analitico = calcular_PC58(zeta, wn)
+    
+    
+    plt.figure()
+    plt.plot(t, y, label="Resposta ao degrau")
+    plt.axhline(1, linestyle='--', color='r', label="Valor final = 1")
+    plt.title("Resposta ao Degrau do PC5.8")
+    plt.xlabel("Tempo (s)")
+    plt.ylabel("Saída y(t)")
+    plt.grid(True)
+    plt.legend()
+    plt.show()
+    print("Resultados do PC5.8:")
+    print(f"Mp = {Mp:.3f}%")
+    print(f"Tp = {Tp:.3f}s")
+    print(f"Ts = {Ts:.3f}s")
+    print("--------------------------------")
+    print(f"Mp analitico = {Mp_analitico:.3f}%")
+    print(f"Tp analitico = {Tp_analitico:.3f}s")
+    print(f"Ts analitico = {Ts_analitico:.3f}s")
+    
+def sistema_PC59():
+    G = ctrl.TransferFunction([10], [1, 10])
+    H = ctrl.TransferFunction([0.5], [10,0.5])
+    T = ctrl.feedback(G, H)
+    return G, H, T
+
+def analisar_PC59():
+    G, H, T = sistema_PC59()
+    t, y = ctrl.step_response(T, np.linspace(0, 30, 2000))
+    info = ctrl.step_info(T)
+    Mp = info['Overshoot']
+    Tp = info['PeakTime']
+    Ts = info['SettlingTime']
+    print("Resultados do PC5.9:")
+    print(f"Máxima ultrapassagem: Mp = {Mp:.3f}%")
+    print(f"Tp = {Tp:.3f}s")
+    print(f"Tempo de acomodação: Ts = {Ts:.3f}s")
+    plt.figure()
+    plt.plot(t, y, label="Resposta ao degrau")
+    plt.axhline(1, linestyle='--', color='r', label="Valor final = 1")
+    plt.title("Resposta ao Degrau do PC5.9")
+    plt.xlabel("Tempo (s)")
+    plt.ylabel("Saída y(t)")
+    plt.grid(True)
+    plt.legend()
+    plt.show()
+    
+def sistema_PC510():
+    s = ctrl.TransferFunction.s
+    G = 10 / (s * (s + 15)* (s + 5))
+    T = ctrl.feedback(G, 1)
+    return G, T
+
+def analisar_PC510():
+    G, T = sistema_PC510()
+    t = np.linspace(0, 20, 2000)
+    r = t
+    t_out, y= ctrl.forced_response(T, T=t, U=r)
+    e = r - y
+    ess_apox = e[-1]
+    
+    t_out, r, y, e, ess_apox = resposta_rampa(T, tempo_final=40)
+    # ganho de velocidade Kv, pra calcular o erro em regime estacionario
+    s = ctrl.TransferFunction.s
+    # Kv = ctrl.dcgain(G*s)
+    # Kv = ctrl.dcgain((G.minreal()) * s)
+    Kv = 10 / (15* 5)
+    ess_teorico = 1/Kv
+    print("Resultados do PC5.10:")
+    print(f"Erro em regime estacionario teorico: {ess_teorico:.3f}")
+    print(f"Erro em regime estacionario aproximado: {ess_apox:.3f}")
+    print("--------------------------------")
+    plt.figure()
+    plt.plot(t_out, y, label="Resposta à rampa")
+    plt.axhline(1, linestyle='--', color='r', label="Entrada rampa (t)")
+    # plt.axhline(ess_apox, linestyle='--', color='g', label=f"Erro em regime estacionario: {ess_apox:.3f}")
+    plt.title("Resposta à Rampa do PC5.10")
+    plt.xlabel("Tempo (s)")
+    plt.ylabel("Saída y(t)")
+    plt.grid(True)
+    plt.legend()
+    plt.show()
+    
+
+    
+
 if __name__ == "__main__":
     # 4.1
     # G_num = [12]
@@ -305,13 +541,13 @@ if __name__ == "__main__":
 
     erro = erro_estacionario(G, tipo="degrau")
     print(f"Erro em regime estacionario para entrada degrau: {erro}")
-    print("resposta ao degrau: y(t) =", yt_func)
-    print("Expressão LaTeX da resposta ao degrau:", yt_latex)
+    #print("resposta ao degrau: y(t) =", yt_func)
+    #print("Expressão LaTeX da resposta ao degrau:", yt_latex)
     # simular_resposta(G_num, G_den, titulo="Resposta ao Degrau do Sistema em Malha Fechada")
 
     # 4.5
-    print("Solução do exercício 4.5:")
-    print("Encontrando o valor de k para 1% < Mp < 10%:")
+    #print("Solução do exercício 4.5:")
+    # print("Encontrando o valor de k para 1% < Mp < 10%:")
     #candidatos = encontrar_k_P45()
     # if len(candidatos) == 0:
     #     print("Nenhum valor de k encontrado com 1% < Mp < 10%")
@@ -321,5 +557,24 @@ if __name__ == "__main__":
     #         print(f"k = {candidato['k']}, Mp = {candidato['Mp']}%")
     #     print("Plotando a resposta ao degrau para o primeiro candidato:")
     #     plotar_P45(candidatos[0]['k'])
-    print("Solução do exercício 4.8:")
-    executar_PC48()
+    #print("Solução do exercício 4.8:")
+    #executar_PC48()
+    # print("Solução do exercício 4.11:")
+    #executar_PC411()
+    
+    # Resposta à rampa
+    # print("\n" + "="*50)
+    # executar_resposta_rampa()
+    
+    #executar_rampa_L()
+    # print("Solução do exercício 5.4:")
+    # T = PC54_sistema()
+    # Mp = calcular_MUP(T)
+    # print(f"Mp calculado = {Mp:.2f}%")
+    # plotar_resposta_PC54()
+    # print("Solução do exercício 5.8:")
+    # simular_PC58()
+    # print("Solução do exercício 5.9:")
+    # analisar_PC59()
+    print("Solução do exercício 5.10:")
+    analisar_PC510()
